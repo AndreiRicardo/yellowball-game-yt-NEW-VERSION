f62bae2c97adef31b2054902940d216bc17443ef
Andrei Ricardo Rodrigues <andreiricardorodrigues@gmail.com>
2020-11-25 20:13:53 -0300

    remove heads

--- ModulesJS/Desenha.js
+++ ModulesJS/Desenha.js
@@ -21,17 +21,16 @@ export function desenha() {
   if (Game.estadoAtual == Game.estados.jogar) Sprites.jogar.desenha(Game.LARGURA / 2 - Sprites.jogar.largura / 2, Game.ALTURA / 2 - Sprites.jogar.altura / 2);
 
   if (Game.estadoAtual == Game.estados.perdeu) {
-<<<<<<< HEAD
     Sprites.perdeu.desenha(Game.LARGURA / 2 - perdeu.largura / 2, Game.ALTURA / 2 - Sprites.perdeu.altura / 2 - Sprites.spriteRecord.altura / 2);
     Sprites.spriteRecord.desenha(Game.LARGURA / 2 - Sprites.spriteRecord.largura / 2, Game.ALTURA / 2 + Sprites.perdeu.altura / 2 - Sprites.spriteRecord.altura / 2 - 25);
     ctx.fillStyle = "#fff";
     ctx.fillText(Game.bloco.score, 375, 390);
-=======
+
     Sprites.perdeu.desenha(Game.LARGURA / 2 - Sprites.perdeu.largura / 2, Game.ALTURA / 2 - Sprites.perdeu.altura / 2 - Sprites.spriteRecord.altura / 2);
     Sprites.spriteRecord.desenha(Game.LARGURA / 2 - Sprites.spriteRecord.largura / 2, Game.ALTURA / 2 + Sprites.perdeu.altura / 2 - Sprites.spriteRecord.altura / 2 - 25);
     Game.ctx.fillStyle = "#fff";
     Game.ctx.fillText(bloco.score, 375, 390);
->>>>>>> 577456fcc04ee24a9c634ac5fec0140b08128412
+
 
     if (Game.bloco.score > Game.record) {
       Sprites.novo.desenha(Game.LARGURA / 2 - 180, Game.ALTURA / 2 + 30);
