7f930bc0e39bf05fa78eeb2cf9a74bee31163ab2
Andrei Ricardo Rodrigues <andreiricardorodrigues@gmail.com>
2020-12-02 10:42:34 -0300

    Update Clique.js

--- ModulesJS/Clique.js
+++ ModulesJS/Clique.js
@@ -16,6 +16,7 @@ export function Clique(event) {
     else if (Game.estadoAtual == Game.estados.jogando) {
         Game.bloco.pula();
     }
+}
      */
     // teste funciona ele pula Game.bloco.pula();
 
@@ -24,5 +25,5 @@ export function Clique(event) {
             Game.obstaculos.limpa(),
             Game.bloco.reset()) : (Game.estadoAtual == Game.estados.jogando) ? Game.bloco.pula() : console.log("num deu");
 
-}
-//
+
+
