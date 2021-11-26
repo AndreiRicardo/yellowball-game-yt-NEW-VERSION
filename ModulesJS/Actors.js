7ffd3e94d7eebb4be36e6202f17cf3fb8b2d4919
Andrei Ricardo Rodrigues <andreiricardorodrigues@gmail.com>
2021-11-26 16:56:35 -0300

    minha cabeça ta quente
    ver de onde vem o sprites

--- ModulesJS/Actors.js
+++ ModulesJS/Actors.js
@@ -1,2 +1,4 @@
 import * as Envy from "./Envy.js";
 
+// de onde vem spritefloor
+
--- ModulesJS/Envy.js
+++ ModulesJS/Envy.js
@@ -5,22 +5,27 @@
 //variaveis globais
 
 export let Objs = {
-    canvasTela: "",
-    Contexto: "",
-    ALTURA: "",
-    LARGURA: "",
-    VELOCIDADE: "",
-    maxPulos: "",
-    estadoAtual: "",
-    record: "",
-    img: "",
-    pontosParaNovaFase: "",
-    faseAtual: "",
-    labelNovaFase: {},
-    estados: {},
-    floor: {},
-    char: {},
-    obstaculos: {}
+    canvasTela:'',
+    Contexto:'',
+    ALTURA:'',
+    LARGURA:'',
+    VELOCIDADE: 6,
+    maxPulos: 3,
+    estadoAtua:'',
+    record:'',
+    img:'',
+    pontosParaNovaFase:'',
+    faseAtual: 0,
+    labelNovaFase:'',
+    estados: {
+        jogar: 0,
+        jogando: 1,
+        perdeu: 2
+    },
+    floor:'',
+    char:'',
+    obstaculos:''
+
 }
 
 // função set variaveis
