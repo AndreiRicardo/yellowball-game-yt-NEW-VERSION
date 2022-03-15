32e9cd53905142ccc823ee00f839424fa2763f9c
AndreiRicardo <andreiricardorodrigues@gmail.com>
2022-03-14 22:43:37 -0300

    opa consigo manipulas a canvas

---
+++ ModulesJS/Screen.js
@@ -0,0 +1,21 @@
+export
+
+var canvas, ctx, LARGURA, ALTURA;
+
+
+ALTURA = window.innerHeight;
+LARGURA = window.innerWidth;
+
+if (LARGURA >= 500) {
+    LARGURA = 600;
+    ALTURA = 600;
+}
+
+canvas = document.createElement("canvas");
+canvas.width = LARGURA;
+canvas.height = ALTURA;
+canvas.style.border = "1px solid #000";
+canvas.style.backgroundColor = "blue";
+
+ctx = canvas.getContext("2d");
+document.body.appendChild(canvas);
