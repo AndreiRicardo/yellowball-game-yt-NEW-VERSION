60b66598e3da3208192df1be33abafa27d5bf91a
Andrei Ricardo Rodrigues <andreiricardorodrigues@gmail.com>
2023-08-03 16:06:14 -0300

    removi os scrolls da tela

--- ModulesJS/Screen.js
+++ ModulesJS/Screen.js
@@ -14,8 +14,9 @@ if (LARGURA >= 500) {
 } */
 
 export function sCreen() {
+    // rever essa config nao pode ficar aqui
   console.log("tem qu sair alguma caralha dessa porra aqui tmbm");
-  // propriendades
+  // propriendades ========================================
   var canvas,
     ctx,
     LARGURA,
@@ -30,15 +31,16 @@ export function sCreen() {
     LARGURA = 1900;
     ALTURA = 910;
   }
-  // configuração
+  // configuração =========================================
 
   canvas = document.createElement("canvas");
-  canvas.width = LARGURA;
-  canvas.height = ALTURA;
+  canvas.style.width = "100%";
+  canvas.style.height = "100%";
   canvas.style.border = "1px solid #000";
   canvas.style.backgroundColor = "blue";
-
   /*   ctx = canvas.getContext("2d"); */ // ainda não sei onde esse cara vai mas pra construção aqui nao preciso
   canvas.getContext("2d");
   document.body.appendChild(canvas);
+  // alterando propriedades do body no braço
+  document.body.style.overflow = "hidden"; // em caso voce queira remover ou deixar os scroll da tela basta manter (scroll removido) ou remover esta linha que devolve as scrolls .
 }
